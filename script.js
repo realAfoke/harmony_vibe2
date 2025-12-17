a3a2ff0d11939717154837efbe27e9c2839125a2
realAfoke <ighoafokereality3@gmail.com>
2025-12-17 14:21:22 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+const audios=document.querySelectorAll(".audio")
+console.log(audios)
+for (const aud of audios){
+    console.log(aud.play)
+}
